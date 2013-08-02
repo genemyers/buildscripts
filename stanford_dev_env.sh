6ace69d6e5365a44d96192094e6a081d5ce18ef7
genemyers <github@datumlogic.com>
2013-08-02 22:36:43 +0000

    added stanford_dev_en.sh

---
+++ stanford_dev_env.sh
@@ -0,0 +1,31 @@
+cd $HOME
+#install Node.js
+sudo apt-get update
+sudo apt-get install python-software-properties python g++ make
+sudo add-apt-repository ppa:chris-lea/node.js
+sudo apt-get install nodejs
+sudo apt-get install npm
+#setup screen
+cd $HOME
+wget raw.github.com/genemyers/dotfiles/master/.screenrc -O .screenrc
+#head .screenrc
+#screen
+# gets and installs GIT command line client
+sudo apt-get install -y git-core
+#install emacs
+git clone https://github.com/genemyers/setup.git 
+./setup/setup.sh
+#setup heroku
+wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
+heroku login
+ssh-keygen –t rsa
+heroku keys:add
+#start screen
+screen
+#clone bitstarter project
+git clone https://github.com/genemyers/bitstarter.git
+cd bitstarter
+#get and install express
+sudo npm install express  #-g doesn’t work, or do I need to add path?
+node web.js
+
