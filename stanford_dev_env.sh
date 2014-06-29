fff87e93ce3f09a996470ad6340403b000c51997
genemyers <github@datumlogic.com>
2014-06-29 18:28:37 +0100

    added code to change machinename

--- stanford_dev_env.sh
+++ stanford_dev_env.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 #put a test here using if [] then to make sure this is called with BASH
+echo Please enter the Machine name (eg: dev-regservice)
+read machinename
+sudo sed -i '/^127.0.0.1/ s/$/ '${machinename}'/'  /etc/hosts
+sudo sed -i 's/.*/'${machinename}'/g' /etc/hostname
+sudo hostname -F /etc/hostname
 cd $HOME
 #install Node.js
 sudo apt-get update
