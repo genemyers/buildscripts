f50e8af8c42893ed6b6c92ea87056767c749fcde
genemyers <github@datumlogic.com>
2014-06-29 19:08:01 +0100

    removed ()'s from echo command

--- machinename.sh
+++ machinename.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo Please enter the Machine name (eg: dev-regservice)
+echo Please enter the Machine name eg: dev-regservice
 read machinename
 sudo sed -i '/^127.0.0.1/ s/$/ '${machinename}'/'  /etc/hosts
 sudo sed -i 's/.*/'${machinename}'/g' /etc/hostname
