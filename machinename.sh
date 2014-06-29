ead89509bd7dac5646ec224c0bef9e29439a1c89
genemyers <github@datumlogic.com>
2014-06-29 18:36:42 +0100

    added script to change machinename

---
+++ machinename.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo Please enter the Machine name (eg: dev-regservice)
+read machinename
+sudo sed -i '/^127.0.0.1/ s/$/ '${machinename}'/'  /etc/hosts
+sudo sed -i 's/.*/'${machinename}'/g' /etc/hostname
+sudo hostname -F /etc/hostname
